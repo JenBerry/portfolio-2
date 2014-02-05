b6ac46973b37707ff2e647a561ada22ca7f76d57
Jen Berry <jenberrymail@gmail.com>
2014-02-05 15:27:51 +0000

    update blog

--- client/client.js
+++ client/client.js
@@ -2,20 +2,31 @@
 * Templates
 */
 Template.blogArticle.blog = function () {
-	return Blog.find({}, {sort: {datetime: -1}});
+	return Blog.find({}, {sort: {date: -1}});
 }
+
+Handlebars.registerHelper('datetime', function(date, format){
+	if (format=="day"){
+		return date.getDate()
+	}
+	if (format=="monthName"){
+		var monthNames = ['Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+		return monthNames[date.getMonth()]
+	}
+	if (format == "date"){
+		var year = date.getFullYear()
+		var month = ("0" + (date.getMonth()+1)).slice(-2)
+		var day = ("0" + date.getDate()).slice(-2)
+		return year+'-'+month+'-'+day;
+	}
+})
 
 Template.addBlog.events = {
 	'click input#submitBlog' : function () {
-
+		
 		var date = new Date();
-		var day = date.getDate();
-		var monthNames = ['Jan','Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-		var month = monthNames[date.getMonth()];
-		var datetime = date.getFullYear()+'-'+date.getMonth()+'-'+day;
 		var title = document.getElementById('blogTitleInput');
 		var text = document.getElementById('blogTextInput');
-
 		var addedBy;
 		if (Meteor.user()){
 			addedBy = Meteor.user().profile.name;
@@ -26,9 +37,7 @@ Template.addBlog.events = {
 
 		if (title.value != '' && text.value != ''){
 			Blog.insert({
-				datetime: datetime,
-				day: day,
-				month: month,
+				date: date,
 				title: title.value,
 				blogText: text.value,
 				addedBy: addedBy,
@@ -36,11 +45,31 @@ Template.addBlog.events = {
 			document.getElementById('blogTitleInput').value = '';
 			document.getElementById('blogTextInput').value = '';
 
-			datetime='';
-			day='';
-			month='';
 			title.value='';
 			text.value='';
+		}
+	}
+}
+
+Template.updateBlog.events = {
+	'click input.updateBlog' : function(){
+		id = this._id;
+		var newDate = new Date(document.getElementById(id+'_date').value);
+		console.log(newDate);
+		var newTitle = document.getElementById(id+'_title').value;
+		var newText = document.getElementById(id+'_text').value;
+
+		if (newTitle.value != '' && newText.value != ''){
+			Blog.update({_id: id},
+				{$set:{
+					date: newDate,
+					title: newTitle,
+					blogText: newText,
+				}}
+			);
+
+			newTitle='';
+			newText='';
 		}
 	}
 }
