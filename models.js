63d2b432aa23699212e2d6ea1e55fe097dfd57fe
Jen Berry <jen@jenberrymail@gmail.com>
2014-03-25 17:02:48 +0000

    remove insecure package

--- models.js
+++ models.js
@@ -25,3 +25,33 @@ Meteor.publish(null, function() {
 		return Projects.find({published : {$nin: [false]}});
 	}
 });
+
+//security
+Blog.allow({
+	'insert': function(userId) {
+		if (userId){return true;}
+		else{return false;}
+	},
+	'update': function(userId) {
+		if (userId){return true;}
+		else{return false;}
+	},
+	'remove': function(userId) {
+		if (userId){return true;}
+		else{return false;}
+	}
+});
+Projects.allow({
+	'insert': function(userId) {
+		if (userId){return true;}
+		else {return false;}
+	},
+	'update': function(userId) {
+		if (userId){return true;}
+		else {return false;}
+	},
+	'remove': function(userId) {
+		if(userId) {return true;}
+		else{return false;}
+	}
+});
