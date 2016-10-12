6ddff17cc9cf1e18f4e24d072153031ed9bc8c78
Jen Berry <jenberrymail@gmail.com>
2016-10-12 12:17:50 +0100

    update javascript to animate new heading

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	// Start the home animation when the document has loaded
-	$('.homeheading, .titleimage, .leaddown div').css({
+	$('.homeheading, .titletext, .titletextsmall, .leaddown div').css({
 		opacity: 1,
 		left:0,
 		top:0
